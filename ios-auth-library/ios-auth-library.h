ca24811f9670353c654411f3088dc14e3c0fa942
David Collom <david.collom@parivedasolutions.com>
2016-10-10 10:35:16 -0500

    Initial Commit

---
+++ ios-auth-library/ios-auth-library.h
@@ -0,0 +1,19 @@
+//
+//  ios-auth-library.h
+//  ios-auth-library
+//
+//  Created by David Collom on 10/10/16.
+//  Copyright © 2016 Pariveda Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ios-auth-library.
+FOUNDATION_EXPORT double ios_auth_libraryVersionNumber;
+
+//! Project version string for ios-auth-library.
+FOUNDATION_EXPORT const unsigned char ios_auth_libraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ios_auth_library/PublicHeader.h>
+
+
