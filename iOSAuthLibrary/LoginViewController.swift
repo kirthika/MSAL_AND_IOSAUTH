35b917f99efeaba1a138f4440c3ff12cb58062a9
David Collom <david.collom@parivedasolutions.com>
2016-10-17 17:30:33 -0500

    Linked LoginViewController to web view

---
+++ iOSAuthLibrary/LoginViewController.swift
@@ -0,0 +1,39 @@
+//
+//  LoginViewController.swift
+//  ios-auth-library
+//
+//  Created by David Collom on 10/11/16.
+//  Copyright Â© 2016 Pariveda Solutions. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet var loginView: UIWebView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let url = NSURL (string: "https://login.microsoftonline.com/tyrtsi.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1_SignupAndSignin&client_Id=ce25c98b-f01d-46ad-936a-62ac28c939e5&nonce=defaultNonce&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob&scope=openid&response_type=id_token&prompt=login");
+        let requestObj = NSURLRequest(URL: url!);
+        loginView.loadRequest(requestObj);
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
