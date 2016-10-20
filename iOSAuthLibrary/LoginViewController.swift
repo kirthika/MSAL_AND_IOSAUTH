fd6da48e268fb69c18f5dca8ae2a28af99f6267e
Toyota Project Team <toyotaprojectteam@TOYOTA-PROJECT-CHI.local>
2016-10-20 13:41:49 -0500

    removed nonce and added response_mode

--- iOSAuthLibrary/LoginViewController.swift
+++ iOSAuthLibrary/LoginViewController.swift
@@ -14,7 +14,7 @@ public class LoginViewController: UIViewController {
     override public func viewDidLoad() {
         super.viewDidLoad()
         
-        let url = NSURL (string: "https://login.microsoftonline.com/tyrtsi.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1_SignupAndSignin&client_Id=ce25c98b-f01d-46ad-936a-62ac28c939e5&nonce=defaultNonce&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob&scope=openid&response_type=id_token&prompt=login");
+        let url = NSURL (string: "https://login.microsoftonline.com/tyrtsi.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1_SignupAndSignin&client_Id=ce25c98b-f01d-46ad-936a-62ac28c939e5&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob&scope=openid&response_type=id_token&response_mode=query");
         let requestObj = NSURLRequest(URL: url!);
         loginView.loadRequest(requestObj);
         // Do any additional setup after loading the view.
