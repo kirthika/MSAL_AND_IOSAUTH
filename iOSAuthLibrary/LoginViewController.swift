ffd9778f5c443529478bd28e060030d5e2cfb704
Toyota Project Team <toyotaprojectteam@TOYOTA-PROJECT-CHI.local>
2016-10-26 16:41:40 -0500

    refactored and removed unnecessary code

--- iOSAuthLibrary/LoginViewController.swift
+++ iOSAuthLibrary/LoginViewController.swift
@@ -22,9 +22,9 @@ open class LoginViewController: UIViewController, UIWebViewDelegate {
         let clientId = "ce25c98b-f01d-46ad-936a-62ac28c939e5"
         let redirectURI = "urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob"
         let scope = "offline_access%20openid"
+        let state = ""
         let responseType = "code%20id_token"
         let responseMode = "query"
-        let prompt = "login"
         
         let url = domain +
             tenant +
@@ -34,9 +34,9 @@ open class LoginViewController: UIViewController, UIWebViewDelegate {
             "&client_id=" + clientId +
             "&redirect_uri=" + redirectURI +
             "&scope=" + scope +
+            "&state=" + state +
             "&response_type=" + responseType +
-            "&response_mode=" + responseMode +
-            "&prompt=" + prompt
+            "&response_mode=" + responseMode
         loginView.loadRequest(URLRequest(url: URL(string: url)!))
         loginView.delegate = self
     }
@@ -46,33 +46,27 @@ open class LoginViewController: UIViewController, UIWebViewDelegate {
     }
     
     open func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
-        print(request.url)
-        print(navigationType)
-        if (navigationType == UIWebViewNavigationType.linkClicked) {
-            print("clickedLink")
-            let url = request.url
-            let scheme = url?.scheme
-            print(scheme)
-            if (scheme == "urn:ietf:wg:oauth:2.0:oob") {
-                print("redirect")
-                // Now handled the url by asking the app to open it
-                return false; // don't let the webview process it.
-            }
-        }
+        // Retrieve code and id token if they exist
         let url = request.url?.absoluteString
         print(url)
+        let redirectRange = url?.range(of: "urn:ietf:wg:oauth:2.0:oob")
+        let stateRange = url?.range(of: "state=")
+        let codeRange = url?.range(of: "&code=")
+        let tokenRange = url?.range(of: "&id_token=")
         
-        // Retrieve code and id token if they exist
-        if (url?.range(of: "urn:ietf:wg:oauth:2.0:oob") != nil && url?.range(of: "code=") != nil && url?.range(of: "&id_token=") != nil) {
-            let codeIndex = url?.range(of: "code=")?.lowerBound
-            let tokenIndex = url?.range(of: "&id_token=")?.lowerBound
-            let auth_code = url?.substring(with: codeIndex!..<tokenIndex!)
-            let id_token = url?.substring(from: tokenIndex!)
+        if (redirectRange != nil && stateRange != nil && codeRange != nil && tokenRange != nil) {
+            let stateUpperIndex = stateRange?.upperBound
+            let codeLowerIndex = codeRange?.lowerBound
+            let codeUpperIndex = codeRange?.upperBound
+            let tokenLowerIndex = tokenRange?.lowerBound
+            let tokenUpperIndex = tokenRange?.upperBound
+            let state = url?.substring(with: stateUpperIndex!..<codeLowerIndex!)
+            let auth_code = url?.substring(with: codeUpperIndex!..<tokenLowerIndex!)
+            let id_token = url?.substring(from: tokenUpperIndex!)
             
-            print(codeIndex)
-            print(tokenIndex)
-            print(auth_code)
-            print(id_token)
+            print(state!)
+            print(auth_code!)
+            print(id_token!)
         }
         return true;
     }
