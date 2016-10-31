f8e50d5e452a32d09e51a2db9d33c87aaa09fbe6
Toyota Project Team <toyotaprojectteam@TOYOTA-PROJECT-CHI.local>
2016-10-31 18:06:06 -0500

    even more testing

--- iOSAuthLibrary/KeychainService.swift
+++ iOSAuthLibrary/KeychainService.swift
@@ -28,13 +28,24 @@ open class KeychainService {
     
     public func getToken(_ tokenType: String) -> String {
         print(tokenType)
-        var id_token: String
         do {
+            try Keychain(service: "com.parivedasolutions.iOSAuthLibrary").set("test", key: "id_token")
+        }
+        catch let error {
+            print(error)
+            print("broken set")
+        }
+        var id_token: String
+        var token: String
+        do {
+            print(Keychain())
             print(Keychain(service: "com.parivedasolutions.iOSAuthLibrary"))
             print(keychain.allKeys())
+            try token = Keychain().get("id_token")!
             try id_token = Keychain(service: "com.parivedasolutions.iOSAuthLibrary").get(tokenType)!
         } catch let error {
             print(error)
+            print("broken get")
             return ""
         }
         return id_token
