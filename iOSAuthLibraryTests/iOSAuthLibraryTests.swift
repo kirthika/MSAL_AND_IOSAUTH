3f053c5e7500f91ab74e36dbcf00d3b39f5a5c7b
Toyota Project Team <toyotaprojectteam@TOYOTA-PROJECT-CHI.local>
2016-11-02 13:11:57 -0500

    Updated tests

--- iOSAuthLibraryTests/iOSAuthLibraryTests.swift
+++ iOSAuthLibraryTests/iOSAuthLibraryTests.swift
@@ -11,9 +11,18 @@ import XCTest
 
 class iOSAuthLibraryTests: XCTestCase {
     
+    var loginViewController: LoginViewController!
+    var authLibrary: AuthLibrary!
+    var keychainService: KeychainService!
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        loginViewController = storyboard.instantiateInitialViewController() as! LoginViewController
+        
+        authLibrary = AuthLibrary()
+        keychainService = KeychainService()
     }
     
     override func tearDown() {
@@ -22,8 +31,7 @@ class iOSAuthLibraryTests: XCTestCase {
     }
     
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(true)
     }
     
     func testPerformanceExample() {
