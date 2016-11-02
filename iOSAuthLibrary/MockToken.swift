7e7f65c323aeae0752134f05230a1248a3966830
Toyota Project Team <toyotaprojectteam@TOYOTA-PROJECT-CHI.local>
2016-11-02 17:10:56 -0500

    Mocked classes for storage and retrieval of tokens

---
+++ iOSAuthLibrary/MockToken.swift
@@ -0,0 +1,15 @@
+//
+//  MockToken.swift
+//  iOSAuthLibrary
+//
+//  Created by Toyota Project Team on 11/2/16.
+//  Copyright © 2016 Pariveda Solutions. All rights reserved.
+//
+
+import Foundation
+
+class MockToken {
+    var id_token: String = ""
+    var auth_token: String = ""
+    var refresh_token: String = ""
+}
--- iOSAuthLibraryTests/iOSAuthLibraryTests.swift
+++ iOSAuthLibraryTests/iOSAuthLibraryTests.swift
@@ -13,7 +13,7 @@ class iOSAuthLibraryTests: XCTestCase {
     
     var loginViewController: LoginViewController!
     var authLibrary: AuthLibrary!
-    var keychainService: KeychainService!
+    var keychainService: MockKeychainService!
     
     override func setUp() {
         super.setUp()
@@ -22,15 +22,25 @@ class iOSAuthLibraryTests: XCTestCase {
         loginViewController = storyboard.instantiateInitialViewController() as! LoginViewController
         
         authLibrary = AuthLibrary()
-        keychainService = KeychainService()
+        keychainService = MockKeychainService()
     }
     
     override func tearDown() {
         super.tearDown()
     }
     
-    func testExample() {
-        XCTAssert(true)
+    func testCorrectMockStoreAndGetToken() {
+        let mockToken = "test"
+        keychainService.storeToken(mockToken, TokenType.id_token.rawValue)
+        let token = keychainService.getToken(TokenType.id_token.rawValue)
+        XCTAssert(token == mockToken)
+    }
+    
+    func testIncorrectMockStoreAndGetToken() {
+        let mockToken = "test"
+        keychainService.storeToken(mockToken, TokenType.id_token.rawValue)
+        let token = keychainService.getToken(TokenType.auth_token.rawValue)
+        XCTAssert(token != mockToken)
     }
     
     func testPerformanceExample() {
