95b4fd20f0b4610015512e81985b741cc5e45614
David Collom <david.m.collom@gmail.com>
2016-11-03 13:25:04 -0500

    testing

--- iOSAuthLibrary/AuthLibrary.swift
+++ iOSAuthLibrary/AuthLibrary.swift
@@ -48,12 +48,13 @@ open class AuthLibrary {
 
     open func getAuthCode() -> String {
         var auth_code = ""
-        let authCodeProps = PListService("authCodeRequest")
-        let request = authProps.getProperty("domain") + "3ca60f43-0cb5-4822-9ca0-1553fc5382c9/" + authProps.getProperty("oauth") + authProps.getProperty("authorize")
+        //let authCodeProps = PListService("authCodeRequest")
+        //let request = authProps.getProperty("domain") + "3ca60f43-0cb5-4822-9ca0-1553fc5382c9/" + authProps.getProperty("oauth") + authProps.getProperty("authorize")
+        let request = "https://login.microsoftonline.com/3ca60f43-0cb5-4822-9ca0-1553fc5382c9/oauth2/v2.0/authorize"
         print(request)
-        Alamofire.request(authProps.getProperty("domain") + "3ca60f43-0cb5-4822-9ca0-1553fc5382c9/" + authProps.getProperty("oauth") + authProps.getProperty("authorize"),
+        Alamofire.request(request,/*authProps.getProperty("domain") + "3ca60f43-0cb5-4822-9ca0-1553fc5382c9/" + authProps.getProperty("oauth") + authProps.getProperty("authorize"),*/
                           method: .get,
-                          parameters: authCodeProps.getAllProperties())
+                          parameters: ["p": "B2C_1_SignupAndSignin", "client_id": "461d17fe-5432-48f0-aa60-6a71813cc065", "redirect_uri":"urn%3Aietf%3Awg%3A2.0%3Aoob", "scope":"openid%20offline_access", "state":"currenttempstate", "response_type":"code", "response_mode":"query","prompt":"login"])
             .responseString { auth_code in print("Response String: \(auth_code.result.value)") }
         print(auth_code)
         return auth_code
