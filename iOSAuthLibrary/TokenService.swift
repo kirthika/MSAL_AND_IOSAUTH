f0141c7b7ba87b6830deb1948b06649392a921b3
Nathan Clemente <nathan.clemente@parivedasolutions.com>
2016-11-15 13:30:52 -0800

    Updated success call for token service

--- iOSAuthLibrary/TokenService.swift
+++ iOSAuthLibrary/TokenService.swift
@@ -19,18 +19,16 @@ open class TokenService {
             ["client_id": "ce25c98b-f01d-46ad-936a-62ac28c939e5", "redirect_uri":"urn:ieft:wg:oauth:2.0:oob", "code":auth_code, "grant_type":"authorization_code", "scope":"openid offline_access"]).responseJSON {
             response in
             switch response.result {
-            case .success(let JSON):
-                    /*let info = String(data: JSON.dataUsingEncoding(using: String.Encoding.utf8)!, encoding: String.Encoding.utf8)!
-                    completion([info])*/
-                    print("it worked")
-                    print(JSON)
-                    completion(token)
+            case .success(let JSON as [String: AnyObject]):
+                print("it worked")
+                print(JSON)
+                print(JSON["token_type"]!)
+                completion(token)
             case .failure(let data):
-                    /*if let errorData = data, let info = String(data: errorData, encoding: String.Encoding.utf8) {
-                        completion([info])
-                    }*/
-                    print("it failed")
-                    print(data)
+                print("it failed")
+                print(data)
+            default:
+                print("default")
             }
         }
     }
