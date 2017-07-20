ebdd36eac330927a125ae70086b5c0af76d6027e
Katie Quinn <katiequinn@KQUINN-MBP-CHI.local>
2017-07-20 10:30:53 -0500

    wip

--- iOSAuthLibrary/MSALAuthLibrary.swift
+++ iOSAuthLibrary/MSALAuthLibrary.swift
@@ -50,22 +50,21 @@ open class MSALAuthLibrary {
                     var errorMsg: String = ""
                     errorMsg = self.handleError(error: error!)
                     // completion(false,errorMsg)
+                    let tempAuthority: String = String(format: endpoint, tenantName, "B2C_1_DefaultPolicy")
+                    if let application = try? MSALPublicClientApplication.init(clientId: self.clientId, authority: tempAuthority){
+                        application.acquireToken(forScopes: self.scopes){ (result, error) in
+                            //do something
+                            
+                        }
+                    } else {
+                        print("error resetting forgotten password")
+                    }
                 }
             }
             print("more testing")
             
         } else {
             // completion(false,"Error instantiating MSAL application")
-        }
-        
-        let tempAuthority: String = String(format: endpoint, tenantName, "B2C_1_DefaultPolicy")
-        if let application = try? MSALPublicClientApplication.init(clientId: self.clientId, authority: tempAuthority){
-            application.acquireToken(forScopes: self.scopes){ (result, error) in
-                //do something
-                
-            }
-        } else {
-            print("error resetting forgotten password")
         }
         
     }
